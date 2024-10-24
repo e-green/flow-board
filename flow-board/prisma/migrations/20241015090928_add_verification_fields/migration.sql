0e41341fbd45808c67b59fb39198b3d6518afe43
Sandeepa Rathnayaka <sandeeparathnayaka20@gmail.com>
2024-10-24 10:12:17 +0530

    Delete flow-board directory

--- flow-board/prisma/migrations/20241015090928_add_verification_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "token_expiration" TIMESTAMP(3),
-ADD COLUMN     "verification_token" TEXT,
-ADD COLUMN     "verified" BOOLEAN NOT NULL DEFAULT false;
