cbdb69df994028ff3c66ee6b71970b17ecbbd2c4
Sandeepa-Rathnayaka <sandeeparathnayaka20@gmail.com>
2024-10-28 12:37:01 +0530

    subTask

--- src/pages/api/task/create-subtask.js
+++ src/pages/api/task/create-subtask.js
@@ -1,19 +1,22 @@
+// pages/api/task/create-subtask.js
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { title, taskId } = req.body;
+    const { title, taskId, parentId } = req.body;
 
     try {
-      const subTask = await prisma.subTask.create({
+      const subtask = await prisma.subTask.create({
         data: {
           title,
-          taskId,
+          taskId: parseInt(taskId),
+          parentId: parentId ? parseInt(parentId) : null,
         },
       });
-      res.status(200).json(subTask);
+      res.status(200).json(subtask);
     } catch (error) {
+      console.error('Error creating subtask:', error);
       res.status(500).json({ error: 'Error creating subtask' });
     }
   } else {
